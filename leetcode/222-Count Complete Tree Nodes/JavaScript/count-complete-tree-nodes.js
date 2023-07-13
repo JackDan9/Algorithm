7dd2d8b9dafba8329fa77ff0ffee8d7249c65733
jackdan <1835812864@qq.com>
2023-07-13 10:36:37 +0800

    :cn: Add leetcode 222 Count Complete Tree Nodes

---
+++ leetcode/222-Count Complete Tree Nodes/JavaScript/count-complete-tree-nodes.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function (root) {
+  // let res = 1;
+
+  // let judge = function(node) {
+  //   if (node.left) {
+  //     res = res + 1;
+  //     judge(node.left);
+  //   }
+
+  //   if (node.right) {
+  //     res = res + 1;
+  //     judge(node.right);
+  //   }
+  // }
+  // if(!root) return 0;
+  // judge(root);
+  // return res;
+  // if (!root) return 0;
+
+  // let res = 1;
+  // while(root && root.left) {
+  //   res = res + 1;
+  //   countNodes
+  // }
+
+  // while(root) {
+  //   while(root.left) {
+  //     countNodes(root.left);
+  //   }
+  //   while (root.right) {
+  //     countNodes(root.right);
+  //   }
+    
+  // }
+
+  return res;
+};
