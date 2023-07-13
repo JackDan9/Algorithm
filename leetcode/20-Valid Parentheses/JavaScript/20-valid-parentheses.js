9313002cff839fa6fa341ff9ec2ef56192c7e521
jackdan <1835812864@qq.com>
2023-07-13 10:17:37 +0800

    :cn: Add leetcode 20 Valid Parentheses

--- leetcode/20-Valid Parentheses/JavaScript/20-valid-parentheses.js
+++ leetcode/20-Valid Parentheses/JavaScript/20-valid-parentheses.js
@@ -3,4 +3,31 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+  if(s.length % 2 === 1) {
+    return false;
+  }
+
+  let map = new Map([
+    [')', '('],
+    [']', '['],
+    ['}', '{']
+  ]);
+
+  let res = [];
+
+  for(let ch of s) {
+
+    if(map.has(ch)) {
+      if (!res.length || res[res.length - 1] !== map.get(ch)) {
+        return false;
+      }
+      res.pop();
+    } else {
+      res.push(ch);
+    }
+  }
+  return !res.length;
 }
+
+let s = "{[]}";
+console.log(isValid(s));
